a21c455891379332e8fa66f5de17cbe8869acd96
Marc Roulias <marc@marcroulias.com>
2017-07-08 16:05:40 -0700

    added more comments to default controller

--- src/frontend-bundle/lib/controller/DefaultController.js
+++ src/frontend-bundle/lib/controller/DefaultController.js
@@ -1,18 +1,30 @@
+/*
+ * This file is part of the conga-full-demo project
+ *
+ * (c) Marc Roulias <marc@lampjunkie.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 const Controller = require('@conga/framework').Controller;
 
 /**
- * This is the default controller which defines the action
- * to render the main index page
- * 
- * @Route("/")
+ * The DefaultController defines the default actions and routes
+ * 🔐
+ *
+ * @Route("/") ◀︎ this prefixes all routes in this controller with "/"
  */
 module.exports = class DefaultController extends Controller {
 
     /**
-     * This is the default index action
+     * @Route("/",              ◀︎ the route path
+     *    name="default.index", ◀︎ unique name to reference route in templates
+     *    methods=["GET"]       ◀︎ the allowed HTTP method(s)
+     * )
      *
-     * @Route("/", name="default.index", methods=["GET"])
-     * @Template
+     * @Template                ◀︎ this tells conga to render the corresponding template
+     *                            from lib/resources/views/default/index.html.twig
      */
     index(req, res) {
         res.return();
